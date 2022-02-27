9aba154cf78a1e10f886a4c291459b693954b116
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-02-27 18:53:14 +0100

    Add hero.module.scss

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,13 +7,13 @@ const List = () => {
           <p>Interesting things I want to check out</p>
           <section>
               <article>
-                  <h2>Books</h2>
+                  <h2 className="title">Books</h2>
               </article>
               <article>
-                  <h2>Movies</h2>
+                  <h2 className="title">Movies</h2>
               </article>
               <article>
-                  <h2>Games</h2>
+                  <h2 className="title">Games</h2>
               </article>
           </section>
       </div>
